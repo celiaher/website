ebddc88917a97af2356a0e840cf46dca9d8a8666
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2022-03-15 22:36:01 -0400

    initial commit
    hero and basic links done

---
+++ main.js
@@ -0,0 +1,11 @@
+$('.nav-trigger').hover(mouseEnter, mouseLeave);
+function mouseEnter() {
+    $(".nav").addClass('nav-hover');
+};
+function mouseLeave() {
+    $(".nav").removeClass('nav-hover');
+};
+$(".nav-title").click(function() {
+    window.scrollTo(0, 0);
+    console.log('aids');
+});
