b0f1e767ddc7b3cf5f6c11fc0ff2f67affc5df8c
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2022-03-18 23:13:49 -0400

    dark mode + nearly done with nav :>

---
+++ scripts/index.js
@@ -0,0 +1,23 @@
+function darkClick()
+{
+    $(".footer").css("background-color", "#EDF2F4");
+    $("body").css("background-color", "#EDF2F4");
+    $(".link-header").css("color", "#2B2D42");
+    $(".footer-line").css("stroke", "#2B2D42");
+    $("#copyright").css("color", "#2B2D42");
+    $(".links-svg-dark").attr("class", "links-svg-light");
+    $(".mode-dark").attr("onclick", "lightClick();");
+    $(".mode-dark").attr("class", "mode-light bi bi-cloud-moon");
+}
+function lightClick()
+{
+    $(".footer").css("background-color", "#303035");
+    $("body").css("background-color", "#303035");
+    $(".link-header").css("color", "#EDF2F4");
+    $(".footer-line").css("stroke", "#EDF2F4");
+    $("#copyright").css("color", "#EDF2F4");
+    $(".links-svg-light").attr("class", "links-svg-dark");
+    $(".mode-light").attr("onclick", "darkClick();");
+    $(".mode-light").attr("class", "mode-dark bi bi-cloud-moon");
+}
+document.getElementById("copyright").innerText += ` ${new Date().getFullYear()}`;
