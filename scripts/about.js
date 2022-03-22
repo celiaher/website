e5234e1f6e2fb9d7f3398678bd867556ef76d189
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2022-03-21 23:46:46 -0400

    about page (still need to add 21-22)

---
+++ scripts/about.js
@@ -0,0 +1,23 @@
+function darkClick()
+{
+    $(".footer").css("background-color", "#EDF2F4");
+    $("body").css("background-color", "#EDF2F4");
+    $(".footer-line").css("stroke", "#2B2D42");
+    $("#copyright").css("color", "#2B2D42");
+    $(".mode-dark").attr("onclick", "lightClick();");
+    $(".mode-dark").attr("class", "mode-light bi bi-cloud-moon");
+    $("h2").css("color", "#2B2D42");
+    $("p").css("color", "#2B2D42");
+}
+function lightClick()
+{
+    $(".footer").css("background-color", "#303035");
+    $("body").css("background-color", "#303035");
+    $(".footer-line").css("stroke", "#EDF2F4");
+    $("#copyright").css("color", "#EDF2F4");
+    $(".mode-light").attr("onclick", "darkClick();");
+    $(".mode-light").attr("class", "mode-dark bi bi-cloud-moon");
+    $("h2").css("color", "#EDF2F4");
+    $("p").css("color", "#EDF2F4");
+}
+document.getElementById("copyright").innerText += ` ${new Date().getFullYear()}`;
